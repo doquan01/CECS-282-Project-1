73951538bcaa1edb38b8a6507b26b668df41056d
doquan01 <55120808+doquan01@users.noreply.github.com>
2020-10-18 16:35:34 -0700

    Created Staff header file

---
+++ Staff.h
@@ -0,0 +1,21 @@
+#ifdef STAFF_H
+#define STAFF_H
+
+#include "Employee.h"
+#include <iostream>
+
+using namespace std;
+
+class Staff: public Employee
+{
+	public:
+		Staff(double r = 160);
+		void setHourlyRate(double r);
+		double getHourlyRate();
+		void putData();
+		void setData();
+		double monthlyEarning();
+	protected:
+		double hourlyRate;
+};
+#endif
