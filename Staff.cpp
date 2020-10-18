250ae707076ae4d7cce92a471ec0885e0807c179
Hunter Lewis <thesupprter@gmail.com>
2020-10-18 15:43:38 -0700

    Created Monthly Earning function

--- Staff.cpp
+++ Staff.cpp
@@ -5,16 +5,19 @@ using namespace std;
 class Staff extends Employee{
     public:
         float hourlyRate;
-        double monthlySalary;
+        double monthlySalary;    
+    float hourlyRate = STAFF_MONTHLY_HOURS_WORKED;
+    double monthlySalary = FACULTY_MONTHY_SALARY;
+    double monthlyEarning(){
+        monthlySalary = monthlySalary  * hourlyRate;
+        return monthlySalary
+    }
+
     void putData(){
         cout << "\nEmployee ID: " << idNumber;
         cout << "\nEmployee Name: " << firstName, lastName;
         cout << "\nBirthday: " << birthDate;
-        cout << "\nFull Time Monthly Salary: " << hourlyRate * STAFF_MONTHLY_HOURS_WORKED;
-        }
-    }   
-    void monthlySalary(){
-
-    }
-
+        cout << "\nFull Time Monthly Salary: " << monthlyEarning();
+    } 
+    
 }
