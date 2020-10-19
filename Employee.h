c363a1a049f24937a7f9c6cc3d3fd771f9944682
doquan01 <55120808+doquan01@users.noreply.github.com>
2020-10-18 17:33:06 -0700

    Added methods to Employee header

--- Employee.h
+++ Employee.h
@@ -8,6 +8,18 @@ using namespace std;
 class Employee{
 	public:
 		Employee();
+		void putData();
+		void setLastName(sting l);
+		void setFirstName(string f);
+		void setID(int id);
+		void setSex();
+		void setBirthDate(string b);
+		string getLastName();
+		string getFirstName();
+		int getID();
+		enum getSex();
+		string getBirthDate();
+		virtual void monthlyEarning();
 	protected:
 		string lastName;
 		string firstName;
