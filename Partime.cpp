8136598f544fdf5787deff0ebd89c30d8071d566
Hunter Lewis <thesupprter@gmail.com>
2020-10-21 14:39:06 -0700

    Finished partime poggo

--- Partime.cpp
+++ Partime.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 #include "Partime.h"
-
+#include "Constants.h"
 using namespace std;
 
 void Partime::setHoursPerWeek(int weekHours){
@@ -10,10 +10,12 @@ int Partime::getHoursPerWeek(){
 	return hoursPerWeek;
 }
 void Partime::putData(){
-	
+	Employee::putData();
+	cout << "Monthly salary: " << monthlyEarning();
+	cout << "Monthly hours worked: " << STAFF_MONTHLY_HOURS_WORKED;
 }
 double Partime::monthlyEarning(){
-	
+	return hourlyRate * hoursPerWeek * 4;
 }
 Partime::Partime(string lastName, string firstName, string id, Sex s, string birthDate, double r, int weekHours):Staff(lastName, firstName, id, s, birthDate, r){
 	hoursPerWeek = weekHours;
