eaae55afa8e88cfc1bf1d051074c490ab89b8ef4
doquan01 <55120808+doquan01@users.noreply.github.com>
2020-10-21 14:58:03 -0700

    Driver

---
+++ Driver.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "Employee.h"
+#include "Staff.h"
+#include "Faculty.h"
+#include "Partime.h"
+#include "Constants.h"
+#include <vector>
+
+using namespace std;
+
+int main(){
+std::vector<Employee*>= {new Staff("Allen", "Paita", "123", M, "2/23/59", 50.00)};
+	
+}
