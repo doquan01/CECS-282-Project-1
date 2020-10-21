c07209cf8e9a495afa00f2344bf4f8c3cf805674
doquan01 <55120808+doquan01@users.noreply.github.com>
2020-10-20 20:41:59 -0700

    Removed comment

--- Education.h
+++ Education.h
@@ -13,7 +13,7 @@ class Education{
 		string getDegree();
 		string getMajor();
 		int getResearch(); 
-		Education(string d, string m, int r);// needs more i think
+		Education(string d, string m, int r);
 	protected:
 		string Degree;
 		string Major;
