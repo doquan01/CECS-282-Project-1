c638cc973755bd7553c0ff2731db5135053860e4
doquan01 <55120808+doquan01@users.noreply.github.com>
2020-10-24 13:11:50 -0700

    did partime total pay and emplyee total pay

--- Driver.cpp
+++ Driver.cpp
@@ -9,7 +9,7 @@
 using namespace std;
 
 int main(){
-    vector<Employee*> work = { 
+vector<Employee*> work = { 
         new Staff("Allen", "Paita", "123", M, "2/23/59", 50.00),
         new Faculty("Johnson","Anne","243", F, "4/27/62", FU, "Ph.D", "Engineering", 3),
         new Staff("Zapata", "Steven", "456", F, "7/12/64", 35.00),
@@ -18,9 +18,22 @@ int main(){
         new Partime("Guzman", "Augusto", "455", F, "8/10/77", 35.00, 30),
         new Faculty("Andrade","Christopher","623", F, "5/22/80", AS, "MS", "Physical Education", 0),
         new Staff("Rios", "Enrique", "789", M, "6/2/70", 40.00),
-        new Partime("Aldaco", "Marque", "945", M, "11/24/88", 20.00, 35) };
-
+        new Partime("Aldaco", "Marque", "945", M, "11/24/88", 20.00, 35)};
+        
+        
+        double totalPart = 0.0;
+        double totalEmploy = 0.0;
+        
     for (int i = 0; i < work.size(); i++) {
         work.at(i)->putData();
+<<<<<<< Updated upstream
+=======
+        if((work.at(i)->getID() == "455") || (work.at(i)->getID() == "678") || (work.at(i)->getID() == "945")){
+            totalPart += work.at(i)->monthlyEarning();
+        }
+        totalEmploy += work.at(i)->monthlyEarning();
+>>>>>>> Stashed changes
     }
+    cout<<"\nTotal monthly salary for all the part-time staff: $"<<totalPart;
+    cout<<"\nTotal monthly salary for all employees: $"<<totalEmploy<<endl;
 }
